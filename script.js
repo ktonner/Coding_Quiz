c3597c6db36271d85032e02ccc9020667189d6c9
Kathleen Tonner <kathleentonner@gmail.com>
2020-03-14 14:28:41 -0400

    Timer set up

--- script.js
+++ script.js
@@ -2,14 +2,26 @@
 //first select timer and button
 var timer = document.getElementById("timer")
 var startButton = document.getElementById("start")
+
+//declare variables
+var secondsLeft = 75;
+timer.textContent = "Time: " + secondsLeft;
+
+// function timerCounter(){
+//     setInterval(function(){
+//         secondsLeft --;
+//         timer.textContent = "Time: " + secondsLeft;
+//     }, 1000);
+// console.log(secondsLeft)}
+
 //make sure it doesn't start until button is clicked
 startButton.addEventListener("click", function(){
 //then make the loop counting down
-var secondsLeft = 75;
 setInterval(function(){
-timer.textContent = secondsLeft;
-secondsLeft --;
-}
-)
-})
+    secondsLeft --;
+    timer.textContent = "Time: " + secondsLeft;
+}, 1000);
+console.log(secondsLeft)})
+
+    
 // display on page
