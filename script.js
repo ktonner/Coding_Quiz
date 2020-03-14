81c083d7f30cff425efd897a3dac3f3af658e482
Kathleen Tonner <kathleentonner@gmail.com>
2020-03-14 13:36:06 -0400

    Button css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //making the timer
 //first select timer
+var timer = document.getElementById("timer")
 //make sure it doesn't start until button is clicked
 //then make the loop counting down
 // display on page
