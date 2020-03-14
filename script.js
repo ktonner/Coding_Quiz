abfc14987848f7281efefbc5e03b313ca2b35384
Kathleen Tonner <kathleentonner@gmail.com>
2020-03-14 13:47:00 -0400

    Working on timer

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //making the timer
-//first select timer
+//first select timer and button
 var timer = document.getElementById("timer")
+var startButton = document.getElementById("start")
 //make sure it doesn't start until button is clicked
+startButton.addEventListener("click", function(){
 //then make the loop counting down
+var secondsLeft = 75;
+setInterval(function(){
+timer.textContent = secondsLeft;
+secondsLeft --;
+}
+)
+})
 // display on page
